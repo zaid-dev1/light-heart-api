279beef26a2d76b6ad230a7a8de04076a6a362de
zaid_dev1 <zaid.wixpatriots@gmail.com>
2025-04-01 22:55:06 +0500

    unlimited timeout

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { Server } from 'http';
 
 dotenv.config();
 
@@ -22,5 +23,11 @@ async function bootstrap() {
   );
 
   await app.listen(3001);
+
+  const server: Server = app.getHttpServer();
+
+  server.timeout = 0;
+  server.keepAliveTimeout = 0;
+
 }
 bootstrap();
